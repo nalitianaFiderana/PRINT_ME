f3b48340f0f7b38657c868fe061944aff1155b2a
nalitianaFiderana <nalitiana.rivontsoa@gmail.com>
2025-11-01 13:15:25 +0100

    adding the new configuration

--- src/print_me/cli.py
+++ src/print_me/cli.py
@@ -1,3 +1,10 @@
 import typer
 
 app = typer.Typer(help="this CLI will print your portfolio in another way")
+
+@app.command()
+def hello(name: str):
+    """
+        Simple command to greet a user by name.
+    """
+    typer.echo(f"Hello, {name}!")
